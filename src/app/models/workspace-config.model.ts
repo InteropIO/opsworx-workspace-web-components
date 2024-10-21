1826d51ee5b4feb7e83571c2104394302a8e3c3d
Svetlin Yordanov <svetlin.yordanov@tick42.com>
2024-10-21 17:46:29 +0300

    Setup IODesktop
    Signed-off-by: Svetlin Yordanov <svetlin.yordanov@tick42.com>

---
+++ src/app/models/workspace-config.model.ts
@@ -0,0 +1,34 @@
+import { IIcon } from "./icon.model";
+
+export interface IMenuCategory {
+    name: string;
+    icon: IIcon;
+    subCategories?: IMenuSubCategory[];
+    workspaceNames?: string[]
+}
+
+export interface IMenuSubCategory {
+    name: string;
+    workspaceNames?: string[]
+}
+
+export interface IFeaturesConfig {
+    hideNotificationsButton?: boolean;
+    hidePendoButton?: boolean
+    hideUserProfile?: boolean;
+    hideMinimizeButton?: boolean;
+    hideMaximizeButton?: boolean;
+    hideCloseButton?: boolean;
+    hideThemeControls?: boolean;
+    hideEnvIndicator?: boolean;
+}
+
+export interface IWorkspaceConfig {
+    version: string;
+    dashboardAppName: string;
+    allowWorkspaceDefinitionChanges: boolean;
+    categories?: IMenuCategory[];
+    features?: IFeaturesConfig;
+}
+
+export const WorkspaceConfigName = 'WorkspaceConfig';
